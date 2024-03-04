a10ec2e934fae63018ec0faef20a9d7d7bd95bdb
claire.laudy <claire.laudy@thalesgroup.com>
2024-03-04 10:47:06 +0100

    feat(ontology preprocessing): Fix any ontology so that it can be used by biocypher.
    Transform classes names by:
    - removing special characters (` `, `%`, `-`)
    - remplace snake casse by camel case
    - adding first letter uppercase
    
    Adds some related tests.

---
+++ tests/test_2ontologies.py
@@ -0,0 +1,33 @@
+
+def main():
+    import yaml
+
+    import pandas as pd
+    import biocypher
+
+    import ontoweaver
+
+    bc = biocypher.BioCypher(
+        biocypher_config_path = "test_2ontologies/biocypher.yaml",
+        schema_config_path = "test_2ontologies/schema.yaml"
+    )
+    # bc.show_ontology_structure()
+
+    table = pd.read_csv("oim.csv")
+
+    with open("oim.yaml") as fd:
+        mapping = yaml.full_load(fd)
+
+    adapter = ontoweaver.tabular.extract_all(table, mapping)
+    assert(adapter)
+
+    assert(adapter.nodes)
+    bc.write_nodes( adapter.nodes )
+
+    assert(adapter.edges)
+    bc.write_edges( adapter.edges )
+
+    bc.write_import_call()
+
+if __name__ == "__main__":
+    main()
