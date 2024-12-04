86eb365dc6fcfd89db22d2cd30d6da814182e2d6
Johann Dreo <johann.dreo@pasteur.fr>
2024-12-04 13:44:52 +0100

    feat(ontoweave): add --parallel

--- src/tools/ontoweave.py
+++ src/tools/ontoweave.py
@@ -25,6 +25,9 @@ if __name__ == "__main__":
     do.add_argument("-s", "--schema", metavar="FILE", default="schema.yaml",
         help="The BioCypher schema file (the one managing node and edge types). [default: %(default)s]")
 
+    do.add_argument("-p", "--parallel", metavar="NB_CORES", default="0",
+        help=f"Number of processor cores to use when processing with multi-threading. `0` means a sequantial processing (no parallelization, the default). Use 'auto' to let {name} do its best to use a good number. [default: %(default)s]")
+
     do.add_argument("-i", "--import-script-run", action="store_true",
         help=f"If passed {name} will call the import scripts created by Biocypher for you.")
 
@@ -46,6 +49,7 @@ if __name__ == "__main__":
     logging.basicConfig(level = asked.log_level)
 
     logging.info("OntoWeave parameters:")
+
     logging.info("\tmappings:")
     mappings = {}
     for data_map in asked.mapping:
@@ -61,11 +65,17 @@ if __name__ == "__main__":
     logging.info(f"\timport-script-run: `{asked.import_script_run}`")
     logging.info(f"\tlog-level: `{asked.log_level}`")
 
+    if asked.parallel == "auto":
+        parallel = min(32, (os.process_cpu_count() or 1) + 4)
+    else:
+        parallel = int(asked.parallel)
+    logging.info(f"\tparallel: `{asked.parallel}`")
+
     # Late import to avoid useless Biocypher's logs when asking for --help.
     import ontoweaver
 
     logging.info(f"Running OntoWeaver...")
-    import_file = ontoweaver.extract_reconciliate_write(asked.config, asked.schema, mappings, separator=asked.prop_sep, affix=asked.type_affix, affix_separator = asked.type_affix_sep)
+    import_file = ontoweaver.extract_reconciliate_write(asked.config, asked.schema, mappings, parallel_mapping=parallel, separator=asked.prop_sep, affix=asked.type_affix, affix_separator = asked.type_affix_sep)
 
     # Output import file on stdout, in case the user would want to capture it.
     print(import_file)
