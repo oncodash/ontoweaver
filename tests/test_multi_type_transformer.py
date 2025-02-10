0450290624e430a2649d84844a63ece84303ff19
Marko Baric <marko.baric@pasteur.fr>
2025-02-10 15:29:56 +0100

    feat(tests):Test for multi type transformers.

---
+++ tests/test_multi_type_transformer.py
@@ -0,0 +1,56 @@
+def test_multi_type_transformer():
+    import yaml
+    import logging
+    from . import testing_functions
+    import shutil
+    import pandas as pd
+    import biocypher
+    import ontoweaver
+    import time
+
+    directory_name = "multi_type_transformer"
+
+    bc = biocypher.BioCypher(
+        biocypher_config_path="tests/" + directory_name + "/biocypher_config.yaml",
+        schema_config_path="tests/" + directory_name + "/schema_config.yaml"
+    )
+
+    logging.debug("Load data...")
+    csv_file = "tests/" + directory_name + "/data.csv"
+    table = pd.read_csv(csv_file)
+
+    logging.debug("Load mapping...")
+    mapping_file = "tests/" + directory_name + "/mapping.yaml"
+    with open(mapping_file) as fd:
+        mapping = yaml.full_load(fd)
+
+    logging.debug("Run the adapter...")
+
+    adapter = ontoweaver.tabular.extract_table(table, mapping)
+
+    assert (adapter)
+
+    time.sleep(1)
+
+    logging.debug("Write nodes...")
+    assert (adapter.nodes)
+    bc.write_nodes(adapter.nodes)
+
+    logging.debug("Write edges...")
+    assert (adapter.edges)
+    bc.write_edges(adapter.edges)
+
+    logging.debug("Write import script...")
+    bc.write_import_call()
+
+    output_dir = testing_functions.get_latest_directory("biocypher-out")
+
+    assert_output_path = "tests/" + directory_name + "/assert_output"
+
+    testing_functions.compare_csv_files(assert_output_path, output_dir)
+
+    shutil.rmtree(output_dir)
+
+
+if __name__ == "__main__":
+    test_multi_type_transformer()
