2e6c5784c5e2682c833f78d68ec14e5d61039ebc
Johann Dreo <johann.dreo@pasteur.fr>
2025-07-25 11:37:02 +0200

    test: full example
    With a homemade ontology, and all files for a minimal example.

---
+++ tests/test_full_example.py
@@ -0,0 +1,40 @@
+def test_full_example():
+    import logging
+    import ontoweaver
+    import biocypher
+    import re
+    import os
+
+    logging.basicConfig(level=logging.DEBUG)
+
+    dir = "full_example"
+
+    data_mapping = {f"tests/{dir}/data.csv": f"tests/{dir}/mapping.yaml"}
+
+    nodes, edges = ontoweaver.extract(filename_to_mapping=data_mapping, affix="none")
+
+    fnodes, fedges = ontoweaver.fusion.reconciliate(nodes, edges, separator=",")
+
+    bc = biocypher.BioCypher(
+        biocypher_config_path = f"tests/{dir}/config.yaml",
+        schema_config_path = f"tests/{dir}/schema.yaml"
+    )
+    bc.show_ontology_structure()
+
+    bc.write_nodes( fnodes )
+    bc.write_edges( fedges )
+    path = bc.write_import_call()
+
+    outdir = os.path.dirname(path)
+    with open(f"{outdir}/biocypher.ttl", 'r') as fd:
+        ttl = fd.read()
+
+    assert("S1" in ttl)
+    assert("S2" in ttl)
+    assert("GA" in ttl)
+    assert("GO" in ttl)
+    assert(":edge" in ttl)
+
+if __name__ == "__main__":
+    test_full_example()
+
