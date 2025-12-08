281e1ee9d388a7b6100c97b646710bd28f528c7f
Claire Laudy <claire.laudy@pasteur.fr>
2025-12-08 20:54:18 +0100

    fix(custom): Fixes missing import ontoweaver types in custom.py

--- tests/custom_transformer/custom.py
+++ tests/custom_transformer/custom.py
@@ -1,6 +1,6 @@
 
 from ontoweaver import transformer, validate
-from ontoweaver types as owtypes
+from ontoweaver import types as owtypes
 
 class OmniPath(transformer.Transformer):
     """Custom end-user transformer, used to create elements for OmniPath KG database."""
