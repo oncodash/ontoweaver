b900f69fd223b0b203e17c7076347c9d1e0e5b99
Johann Dreo <johann.dreo@pasteur.fr>
2025-12-18 09:49:34 +0100

    fix(tests): use ontoweave's shebang in tests

--- tests/test_ontoweave.py
+++ tests/test_ontoweave.py
@@ -15,7 +15,7 @@ class user_transformer(ontoweaver.transformer.Transformer):
 
 def test_ontoweave():
     logging.debug(f"From: {os.getcwd()}")
-    cmd="uv run ontoweave --biocypher-config ./tests/simplest/biocypher_config.yaml --biocypher-schema ./tests/simplest/schema_config.yaml --type-affix suffix --type-affix-sep : --prop-sep ';' --log-level DEBUG ./tests/simplest/data.csv:./tests/simplest/mapping.yaml"
+    cmd="./bin/ontoweave --biocypher-config ./tests/simplest/biocypher_config.yaml --biocypher-schema ./tests/simplest/schema_config.yaml --type-affix suffix --type-affix-sep : --prop-sep ';' --log-level DEBUG ./tests/simplest/data.csv:./tests/simplest/mapping.yaml"
 
     logging.debug(f"Run: {cmd}")
     subprocess.run(cmd.split(), capture_output=True, check=True)
@@ -23,11 +23,10 @@ def test_ontoweave():
 
 def test_ontoweave_register():
     logging.debug(f"From: {os.getcwd()}")
-    cmd="uv run ontoweave --biocypher-config ./tests/simplest/biocypher_config.yaml --biocypher-schema ./tests/simplest/schema_config.yaml --type-affix suffix --type-affix-sep : --prop-sep ';' --log-level DEBUG ./tests/simplest/data.csv:./tests/simplest/mapping.yaml --register ./tests/test_ontoweave.py"
+    cmd="./bin/ontoweave --biocypher-config ./tests/simplest/biocypher_config.yaml --biocypher-schema ./tests/simplest/schema_config.yaml --type-affix suffix --type-affix-sep : --prop-sep ';' --log-level DEBUG ./tests/simplest/data.csv:./tests/simplest/mapping.yaml --register ./tests/test_ontoweave.py"
 
     logging.debug(f"Run: {cmd}")
     subprocess.run(cmd.split(), capture_output=True, check=True)
-
 
 
 if __name__ == "__main__":
--- tests/test_validate_input.py
+++ tests/test_validate_input.py
@@ -6,7 +6,7 @@ def test_validate_input():
     logging.basicConfig(level=logging.DEBUG)
 
     logging.debug(f"From: {os.getcwd()}")
-    cmd = "uv run ontoweave ./tests/validate_input/data.csv:./tests/validate_input/mapping.yaml --validate-only"
+    cmd = "./bin/ontoweave ./tests/validate_input/data.csv:./tests/validate_input/mapping.yaml --validate-only"
 
     logging.debug(f"Run: {cmd}")
 
