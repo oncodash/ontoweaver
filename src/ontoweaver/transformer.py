066c0e79a7068f69f02322ee0b67351e3fbe952d
Johann Dreo <johann.dreo@pasteur.fr>
2026-02-24 13:05:01 +0100

    fix(translate): typo

--- src/ontoweaver/transformer.py
+++ src/ontoweaver/transformer.py
@@ -768,14 +768,14 @@ class translate(base.Transformer):
 
                 self.translate = {}
                 for i,row in self.df.iterrows():
-                    from = row[self.translate_from]
+                    frm = row[self.translate_frm]
                     to = row[self.translate_to]
-                    if from in self.translate and self.translate[from] != to:
-                        logger.warning(f"The key `{from}` already exists in the translation table, and translated to `{self.translate[from]}`. It now translates to `{to}`. You may want to avoid such duplicates in translation tables.")
-                    if from and to:
-                        self.translate[from] = to
+                    if frm in self.translate and self.translate[frm] != to:
+                        logger.warning(f"The key `{frm}` already exists in the translation table, and translated to `{self.translate[frm]}`. It now translates to `{to}`. You may want to avoid such duplicates in translation tables.")
+                    if frm and to:
+                        self.translate[frm] = to
                     else:
-                        logger.warning(f"Cannot translate from `{self.translate_from}` to `{self.translate_to}`, invalid translations values at row {i} of file `{self.translations_file}`: `{from}` => `{to}`. I will ignore this translation.")
+                        logger.warning(f"Cannot translate frm `{self.translate_frm}` to `{self.translate_to}`, invalid translations values at row {i} of file `{self.translations_file}`: `{frm}` => `{to}`. I will ignore this translation.")
 
         else:
             self.error(f"When using a {type(self).__name__} transformer, you must define either `translations` or `translations_file`.", section="translate.init", exception = exceptions.TransformerInterfaceError)
