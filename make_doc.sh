6c570d925b5f2ce2144cc12868238235bd7db73d
Johann Dreo <johann.dreo@pasteur.fr>
2026-07-22 17:13:19 +0200

    WIP against RTD

--- make_doc.sh
+++ make_doc.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-rm -rf doc_built
+
+if [[ -z $READTHEDOCS_OUTPUT ]] ; then
+    READTHEDOCS_OUTPUT="doc_built"
+fi
+
+echo "Remove output directory: $READTHEDOCS_OUTPUT" >&2
+rm -rf $READTHEDOCS_OUTPUT
 
 echo "Extract keywords" >&2
 keywords="$(./docs/keywords_from_code.py)"
@@ -7,4 +13,5 @@ echo "$keywords" >&2
 cat docs/sections/mapping_api.TPL.rst | sed "s/{{{KEYWORDS}}}/$keywords/g" > docs/sections/mapping_api.rst
 echo "Done" >&2
 
-uv run sphinx-build --builder html docs/ doc_built $*
+uv run sphinx-build --builder html docs/ $READTHEDOCS_OUTPUT $*
+
