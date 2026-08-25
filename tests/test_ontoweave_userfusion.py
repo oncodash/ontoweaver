dd65582c55ccd3e8392c7ee4095eebb0cd0cf3a3
nojhan <nojhan@nojhan.net>
2026-08-25 22:04:49 +0200

    fix(tests): run test_ontoweave_userfusion with repo uv

--- tests/test_ontoweave_userfusion.py
+++ tests/test_ontoweave_userfusion.py
@@ -4,8 +4,16 @@ import subprocess
 
 def test_userfusion():
     logging.debug(f"From: {os.getcwd()}")
-    cmd="tests/userfusion.py --biocypher-config ./tests/simplest/biocypher_config.yaml --biocypher-schema ./tests/simplest/schema_config.yaml --type-affix suffix --type-affix-sep : --prop-sep ';' --debug ./tests/simplest/data.csv:./tests/simplest/mapping.yaml"
+    cmd="uv run tests/userfusion.py --biocypher-config ./tests/simplest/biocypher_config.yaml --biocypher-schema ./tests/simplest/schema_config.yaml --type-affix suffix --type-affix-sep : --prop-sep ';' --debug ./tests/simplest/data.csv:./tests/simplest/mapping.yaml"
 
-    logging.debug(f"Run: {cmd}")
-    subprocess.run(cmd.split(), capture_output=True, check=True)
-
+    logging.info(f"Run: {' '.join(cmd)}")
+    try:
+        subprocess.run(cmd.split(), capture_output=True, check=True)
+    except subprocess.CalledProcessError as e:
+        for k,v in e.__dict__.items():
+            logging.error(k)
+            if isinstance(v, bytes):
+                logging.error(v.decode("utf-8"))
+            else:
+                logging.error(v)
+        raise e
--- tests/userfusion.py
+++ tests/userfusion.py
@@ -1,24 +1,3 @@
-#!/usr/bin/env -S uv run --script
-# /// script
-# requires-python = "<4.0,>=3.12"
-# dependencies = [
-#    "biocypher<1.0.0,>=0.11.0",
-#    "pooch<2.0.0,>=1.7.0",
-#    "pandas<3.0.0,>=2.3.1",
-#    "numpy<3.0.0,>=2.2.4",
-#    "owlready2<1.0,>=0.49",
-#    "jsonargparse<5.0,>=4.39",
-#    "xdg-base-dirs<7.0.0,>=6.0.2",
-#    "pandera[io]<1.0.0,>=0.27.0",
-#    "alive-progress<4.0,>=3.2",
-#    "fsspec<2026.0.0,>=2025.10.0",
-#    "natsort>5.0.0",
-#    "lxml>6.0.0",
-#    "jmespath>=1.0.1",
-#    "openpyxl>=3.1.5",
-# ]
-# ///
-
 import os
 import sys
 import logging
